6f8889963f54d988711f202bb0bbfa6c7f648a7c
SAMBIT CHAKRABORTY <smbtchakraborty@gmail.com>
2023-02-27 14:20:34 +0530

    Co-authored-by: PARTHIB KUMAR DEB <PARTHIB-DEB@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,5 +1,57 @@
 # Student Management System (SMS) - Python Project using Tkinter GUI
 
+# Importing Modules
+from tkinter import *
+import sqlite3
+
+# Creating instance of TK
+root = Tk()
+
+# Connecting to the database
+connectDB = sqlite3.connect('student.db')
+cusorDB = connectDB.cursor()
+cusorDB.execute('create table if not exists student_details (roll_no integer primary key, name text, age integer, gender text, address text, phone integer)')
+cusorDB.commit()
+
+# Defining the variables
+roll_no = IntVar()
+name = StringVar()
+age = IntVar()
+gender = StringVar()
+address = StringVar()
+phone = IntVar()
+
+
+# GUI Rendering code-block
+root.title("Student Management System")
+root.geometry("1350x700+0+0")
+
+# Frames
+Manage_Frame = Frame(root, bd=4, relief=RIDGE, bg="crimson")
+Manage_Frame.place(x=20, y=100, width=450, height=580)
+
+Detail_Frame = Frame(root, bd=4, relief=RIDGE, bg="crimson")
+Detail_Frame.place(x=500, y=100, width=800, height=580)
+
+# Labels
+lbl_title = Label(Manage_Frame, text="Manage Students", bg="crimson", fg="white", font=("times new roman", 30, "bold"))
+lbl_title.grid(row=0, columnspan=2, pady=20)
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+'''
+# Student Management System (SMS) - Python Project using Tkinter GUI
+
 # Importing Required Modules
 from tkinter import *
 from tkinter import ttk
@@ -14,13 +66,12 @@ if not os.path.exists('Student.db'):
     # Create a cursor to execute SQL commands
     c = conn.cursor()
     # Create table - Students
-    c.execute('''CREATE TABLE students ( 
+    c.execute("""CREATE TABLE students ( 
             student_id INTEGER PRIMARY KEY,
             first_name TEXT,
             last_name TEXT,
             email TEXT,
-
-            ''')
+            """    )
     # Commit our command
     conn.commit()
     # Close our connection
@@ -56,4 +107,6 @@ class Student:
         
 
 
-root.mainloop()
+
+conn.mainloop()
+'''
